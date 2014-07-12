b9a1381dee55c5515f13c9666b4a49c358ee39a2
orbitz <orbitz@gmail.com>
2014-07-12 20:59:36 +0200

    Add a context value to transport to make the implementation less stateful

--- lib/scow/scow_server_candidate.ml
+++ lib/scow/scow_server_candidate.ml
@@ -7,7 +7,12 @@ module Make =
       functor (Vote_store : Scow_vote_store.T) ->
         functor (Transport : Scow_transport.T) ->
 struct
+
+  module Msg = Scow_server_msg.Make(Log)(Transport)
+  module TMsg = Scow_transport.Msg
+
   let handle_call _self _state _msg =
     failwith "nyi"
+
 end
 
