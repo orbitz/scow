5d6ea1d2dbf45dda0b763befdc943f14944f4d97
orbitz <orbitz@gmail.com>
2014-07-23 15:37:15 +0200

    Add scaffolding for doing append entries

--- lib/scow/scow_server_leader.ml
+++ lib/scow/scow_server_leader.ml
@@ -19,6 +19,66 @@ struct
       | Ok anything -> Deferred.return (Ok anything)
       | Error _     -> Deferred.return (Error ())
 
+  let is_node_equal n1 n2 = Transport.Node.compare n1 n2 = 0
+
+  let rec send_until' max_par f nodes test in_flight remaining_nodes results =
+    match (in_flight, remaining_nodes) with
+      | ([], []) ->
+        Deferred.return (results, [])
+      | (in_flight, n::ns) when List.length in_flight < max_par ->
+        let missing_amount = max_par - List.length in_flight in
+        let additional =
+          List.map
+            ~f:(fun n -> (n, f n >>= fun result -> Deferred.return (n, result)))
+            (List.take remaining_nodes missing_amount)
+        in
+        let remaining_nodes = List.drop remaining_nodes missing_amount in
+        let in_flight = in_flight @ additional in
+        send_until' max_par f nodes test in_flight remaining_nodes results
+      | (in_flight, remaining_nodes) ->
+        Deferred.any (List.map ~f:snd in_flight)
+        >>= fun (node, result) ->
+        let in_flight = List.Assoc.remove ~equal:is_node_equal in_flight node in
+        let results = (node, result)::results in
+        if test results then
+          Deferred.return (results, in_flight)
+        else
+          send_until' max_par f nodes test in_flight remaining_nodes results
+
+  let send_until ~max_par ~f ~nodes ~test =
+    send_until' max_par f nodes test [] nodes []
+
+  let send_majority_sync max_par f nodes =
+    let test results =
+      let length =
+        results
+        |> List.map ~f:snd
+        |> List.filter ~f:Result.is_ok
+        |> List.length
+      in
+      length > List.length nodes / 2
+    in
+    send_until ~max_par ~f ~nodes ~test
+    >>= fun (results, in_flight) ->
+    let (success, failed) =
+      List.partition_tf
+        ~f:(Fn.compose Result.is_ok snd)
+        results
+    in
+    Deferred.return (success, failed, in_flight)
+
+  let send_majority max_par sync async nodes =
+    send_majority_sync max_par sync nodes
+    >>= function
+      | (success, [], in_flight) ->
+        failwith "nyi"
+      | ([], failed, []) ->
+        (* Everything failed *)
+        failwith "nyi"
+      | (success, failed, in_flight) ->
+        failwith "nyi"
+
+
   let handle_rpc_append_entries self state (node, append_entries, ctx) =
     let module Ae = Scow_rpc.Append_entries in
     if Scow_term.compare (State.current_term state) append_entries.Ae.term < 0 then
@@ -66,7 +126,7 @@ struct
       Deferred.return (Ok state)
     end
 
-  let handle_append_entries self state ret =
+  let handle_append_entries self state ret entries =
     failwith "nyi"
 
   let handle_timeout self state =
@@ -77,8 +137,8 @@ struct
       ignore_error (handle_rpc_append_entries self state (node, append_entries, ctx))
     | Msg.Rpc (TMsg.Request_vote (node, request_vote), ctx) ->
       ignore_error (handle_rpc_request_vote self state (node, request_vote, ctx))
-    | Msg.Append_entries (ret, _) ->
-      ignore_error (handle_append_entries self state ret)
+    | Msg.Append_entries (ret, entries) ->
+      ignore_error (handle_append_entries self state ret entries)
     | Msg.Election_timeout
     | Msg.Heartbeat ->
       ignore_error (handle_timeout self state)
