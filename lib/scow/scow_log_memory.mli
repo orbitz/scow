cf8b7847ad568f420bba617d311d90ff677532fe
orbitz <orbitz@gmail.com>
2014-07-31 07:25:55 +0200

    Add initial memory log

---
+++ lib/scow/scow_log_memory.mli
@@ -0,0 +1,33 @@
+open Async.Std
+
+type elt = Scow_transport_memory.elt
+type t
+
+val create : unit -> t
+
+val append :
+  t ->
+  Scow_term.t ->
+  elt list ->
+  (Scow_log_index.t, [> `Append_failed | `Invalid_log ]) Deferred.Result.t
+
+val get_entry :
+  t ->
+  Scow_log_index.t ->
+  ((Scow_term.t * elt), [> `Not_found | `Invalid_log ]) Deferred.Result.t
+
+val get_term :
+  t ->
+  Scow_log_index.t ->
+  (Scow_term.t, [> `Not_found | `Invalid_log ]) Deferred.Result.t
+
+val get_log_index_range :
+  t ->
+  ((Scow_log_index.t * Scow_log_index.t), [> `Invalid_log ]) Deferred.Result.t
+
+val delete_from_log_index :
+  t ->
+  Scow_log_index.t ->
+  (unit, [> `Invalid_log | `Not_found ]) Deferred.Result.t
+
+val is_elt_equal : elt -> elt -> bool
